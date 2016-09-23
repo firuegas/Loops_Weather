282d6e6ae3a2f5b8f3f13917ca8c4303a95a35d3
Fernanda Bocardo <firuegas@i21a-s2.ufhpc>
2016-09-23 12:57:21 -0400

    Change to make file for max temps of each year then make file with max temp for all years then print 5 max and min

--- MaxTemp_Day.sh
+++ MaxTemp_Day.sh
@@ -2,19 +2,37 @@
 #Scrip to get the highest temperature for each day in August for one year (data was only downloaded for month of August)
 
 month=08
+mkdir years
 for year in {1973..2016} #For each year between 1943 and 2016
 do
-
+		
 	for day in {1..31} #For each day in the month (Always using August, so 31 is OK here, need to modify is not August)
-	do  
+	do  	
 		if [ $day -lt 10 ]	#Add a "0" to the front of single digit days
 		then
 		day=0$day
 		fi
-Date=$year$month$day
-MaxTemp=`awk -F',' '{print $2}' /ufrc/zoo6927/share/PS1.3/data/$year/GNV_$Date.txt | sort -n | tail -n1`
+
+	Date=$year$month$day
+	MaxTemp=`awk -F',' '{print $2}' /ufrc/zoo6927/share/PS1.3/data/$year/GNV_$Date.txt | sort -n | tail -n1`
+
+	echo $year,$MaxTemp >>years/maxtemp$year.txt
   
-echo The max temp for $year 08 $day is $MaxTemp
-  
+	done
+
 done
+
+for mtyear in {1973..2016}
+do
+
+MaxAnual=`awk -F',' '{print $2}' /ufrc/zoo6927/firuegas/MaxTemp/years/maxtemp$mtyear.txt | sort -n | tail -n1`
+
+echo $mtyear,$MaxAnual >>MaxYear.txt
 done
+
+echo The five years with hoter temperatures were
+sort -k2 -t"," -n /ufrc/zoo6927/firuegas/MaxTemp/MaxYear.txt | tail -n5
+
+echo The five years with lower temperatures were $Min
+sort -k2 -t"," -n /ufrc/zoo6927/firuegas/MaxTemp/MaxYear.txt | head -n5
+
